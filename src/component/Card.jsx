f3096e99cc59b2aab81aa03ea3e3a693ecac1aa9
Daniel Ayeni <ayenibabajidedaniel@gmail.com>
2022-05-28 01:09:43 +0100

    Made the images clickable

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -4,7 +4,7 @@ export default function Card(props) {
   return (
     <div className="card">
       <div className="card-item">
-      <img src={props.item.coverImage}  alt="Location" className="card-photo" />
+     <a href={props.item.imageUrl} target="_blank" rel="noreferrer"><img src={props.item.coverImage}  alt="Location" className="card-photo" /></a>
         <div className="card-body">
           <p className="card-location"><img src={locationIcon} alt="" className="location-icon" /><span className="location">{props.item.location}</span><span><a href={props.item.googleMapsUrl} target="_blank" rel="noreferrer" className="location-link">View on Google Maps</a></span></p>
           <h1 className="card-title">{props.item.title}</h1>
